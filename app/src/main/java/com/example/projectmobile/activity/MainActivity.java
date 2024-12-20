93b99a9f5729834e3517f3f6a968fccfa6b9807c
Anhngoc2912 <ngocanhdhkt764@gmail.com>
2024-12-20 18:52:00 +0700

    vuốt

--- app/src/main/java/com/example/projectmobile/activity/MainActivity.java
+++ app/src/main/java/com/example/projectmobile/activity/MainActivity.java
@@ -14,13 +14,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
-import com.example.projectmobile.Fragment.DepthPageTransformer;
+
 import com.example.projectmobile.R;
 import com.example.projectmobile.adapter.ViewPager;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,9 +43,7 @@ public class MainActivity extends AppCompatActivity {
         ViewPager myadapter = new ViewPager(this);
         viewPager2.setAdapter(myadapter);
 
-        //viewPager2.setPageTransformer(new DepthPageTransformer());//hiệu ứng chuyển màn
-        // Tắt hiệu ứng chuyển màn
-        viewPager2.setPageTransformer(null);
+        viewPager2.setUserInputEnabled(false); // Tắt hiệu ứng chuyển trang
 
         //click vào item bottom nav
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -73,35 +70,6 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
-       /* //vuốt màn hình
-        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-
-                switch (position) {
-
-                    case 0:
-                        bottomNavigationView.getMenu().findItem(R.id.home).setChecked(true);
-                        break;
-
-                    case 1:
-                        bottomNavigationView.getMenu().findItem(R.id.dashboard).setChecked(true);
-                        break;
-
-                    case 2:
-                        bottomNavigationView.getMenu().findItem(R.id.user).setChecked(true);
-                        break;
-
-                    case 3:
-                        bottomNavigationView.getMenu().findItem(R.id.setting).setChecked(true);
-                        break;
-                }
-            }
-        });*/
-
-
     }
 
 }
